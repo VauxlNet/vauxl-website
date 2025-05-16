a0c6ee1796041a2db957f709f8791d0fe8d8a0ac
ZPHNX <surreyjesper@gmail.com>
2025-05-16 13:52:49 +0200

    color and added a div in the top -> now error is in middle

--- src/routes/error_404.rs
+++ src/routes/error_404.rs
@@ -4,10 +4,15 @@ use dioxus::prelude::*;
 pub fn Error404(route: Vec<String>) -> Element {
     rsx! {
         div{
-            class:"flex-grow items-center mx-auto",
+                class:"flex-grow",
+        }
+        div{
+            class:"flex-none items-center mx-auto bg-main-400 rounded-lg p-4 text-main-100",
             h1 { "Page not found" }
             p { "We are terribly sorry, but the page you requested doesn't exist." }
-            pre { color: "red", "log:\nattemped to navigate to: {route:?}" }
+            pre { 
+                color: "red", "log:\nattemped to navigate to: {route:?}" 
+            }
         }
     }
 }
