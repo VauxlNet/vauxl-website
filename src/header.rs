2dc7fec3a3569e370334cd032dd7b0ab582d6842
ZPHNX <surreyjesper@gmail.com>
2025-05-19 15:35:47 +0200

    editing the side menu to work properly -> buttons for open and closing works -> buttons for contact menu works => needs to be beautified

--- src/header.rs
+++ src/header.rs
@@ -8,13 +8,15 @@ const ICON: Asset = asset!("/assets/vauxl_icon03.png");
 #[component]
 pub fn Header() -> Element {
     let mut is_contact_menu_open = use_signal(|| false);
+    let mut is_mobile_menu_open = use_signal(|| false);
+    
     let mut close_task_handle: Signal<Option<Task>> = use_signal(|| None);
 
     rsx! {
         header {
             class: "bg-main-100 text-main-300 shadow-md font-main",
             nav {
-                class: "mx-auto flex max-w-7xl items-center justify-between px-4 lg:px-8",
+                class: "mx-auto flex items-center justify-between px-4 lg:px-8",
                 aria_label: "Global",
                 div{
                     class: "flex lg:flex-1",
@@ -39,8 +41,9 @@ pub fn Header() -> Element {
                 div {
                     class: "flex lg:hidden",
                     button {
-                        type: "button",
+                        r#type: "button",
                         class: "-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-300",
+                        onclick: move |_| is_mobile_menu_open.set(true),
                         span {
                             class: "sr-only",
                             "OpenMainMenu",
@@ -151,139 +154,145 @@ pub fn Header() -> Element {
                             },
                         }
                     },
-                    div {
-                        class: "hidden lg:flex lg:flex-1 lg:justify-end",
-                        a {
-                            class: "rounded-lg px-3 py-2 mx-6 font-bold hover:bg-main-200 transition duration-300",
-                            href: "/login",
-                            "LogIn",
-                            span {
-                                class: "aria-hidden hidden",
-                                "&rarr;",
-                            },
+                },
+                div {
+                    class: "hidden lg:flex lg:flex-1 lg:justify-end",
+                    a {
+                        class: "rounded-lg px-3 py-2 mx-2 font-bold hover:bg-main-200 transition duration-300",
+                        href: "/login",
+                        "LogIn",
+                        span {
+                            class: "aria-hidden hidden",
+                            "&rarr;",
                         },
                     },
                 },
             },
-            //TODO: working opening and closing of nav bar window
-            div {
-                class: "lg:hidden text-main-300",
-                role: "dialog",
-                aria_modal: "true",
+            if is_mobile_menu_open() {
                 div {
-                    class: "fixed inset-0 z-10",
-                },
-                div {
-                    class: "fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-main-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10",
-                    div{
-                        class: "flex lg:flex-1",
-                        a {
-                            class: "mr-2 my-3",
-                            href: "/",
-                            span{
-                                class: "sr-only",
+                    class: "lg:hidden text-main-300",
+                    role: "dialog",
+                    aria_modal: "true",
+                    div {
+                        class: "fixed inset-0 z-10",
+                        onclick: move |_| is_mobile_menu_open.set(false),
+                    },
+                    div {
+                        class: "fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-main-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10",
+                        div{
+                            class: "flex lg:flex-1",
+                            a {
+                                class: "mr-2 my-3",
+                                href: "/",
+                                span{
+                                    class: "sr-only",
+                                    "VauxlNet",
+                                },
+                                img {class: "mask-origin-content h-8 w-auto",src: ICON,
+                                },
+                            },
+                            a {
+                                class: "text-xl font-bold my-3",
+                                href: "/",
                                 "VauxlNet",
                             },
-                            img {class: "mask-origin-content h-8 w-auto",src: ICON,
+                        },
+                        button {
+                            r#type:"button",
+                            class: "-m-2.5 rounded-md p-2.5 text-gray-300",
+                            onclick: move |_| is_mobile_menu_open.set(false),
+                            span {
+                                class: "sr-only",
+                                "CloseMenu",
+                            }
+                            svg {
+                                class: "size-6 aria-hidden data-slot",
+                                fill: "none",
+                                view_box: "0 0 24 24",
+                                stroke_width: "1.5",
+                                stroke: "currentColor",
+                                path {
+                                    stroke_linecap: "round",
+                                    stroke_linejoin: "round",
+                                    d: "M6 18 18 6M6 6l12 12",
+                                },
                             },
                         },
-                        a {
-                            class: "text-xl font-bold my-3",
-                            href: "/",
-                            "VauxlNet",
-                        },
-                    },
-                    button {
-                        type:"button",
-                        class: "-m-2.5 rounded-md p-2.5 text-gray-300",
-                        span {
-                            class: "sr-only",
-                            "CloseMenu",
-                        }
-                        svg {
-                            class: "size-6 aria-hidden data-slot",
-                            fill: "none",
-                            view_box: "0 0 24 24",
-                            stroke_width: "1.5",
-                            stroke: "currentColor",
-                            path {
-                                stroke_linecap: "round",
-                                stroke_linejoin: "round",
-                                d: "M6 18 18 6M6 6l12 12",
-                            },
-                        },
-                    },
-                },
-                div {
-                    class: "mt-6 flow-root",
-                    div {
-                        class: "-my-6 divide-y divide-gray-500/10",
                         div {
-                            class: "space-y-2 py-6",
-                            a {
-                                class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
-                                href: "/team",
-                                "Team"
-                            },
-                            a {
-                                class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
-                                href: "/project",
-                                "Projects"
-                            },
-                            a {
-                                class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
-                                href: "/dashboard",
-                                "Dashboard"
-                            },
+                            class: "mt-6 flow-root",
                             div {
-                                class: "-mx-3",
-                                button {
-                                    type: "button",
-                                    class: "flex w-full items-center justify-between aria-controls-dis-1 aria-expanded-f rounded-lg py-2 pr-3.5 pl-3 font-bold hover:bg-main-200",
-                                    "Contact"
-                                    svg {
-                                        class: "size-5 flex-none text-gray-400 aria-hidden data-slot",
-                                        view_box: "0 0 20 20",
-                                        fill: "currentColor",
-                                        path {
-                                            fill_rule: "evenodd",
-                                            d:"M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z",
-                                            clip_rule: "evenodd",
+                                class: "-my-6 divide-y divide-gray-500/10",
+                                div {
+                                    class: "space-y-2 py-6",
+                                    a {
+                                        class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
+                                        href: "/team",
+                                        "Team"
+                                    },
+                                    a {
+                                        class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
+                                        href: "/project",
+                                        "Projects"
+                                    },
+                                    a {
+                                        class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
+                                        href: "/dashboard",
+                                        "Dashboard"
+                                    },
+                                    div {
+                                        class: "block rounded-lg px-3 py-2 font-medium hover:bg-main-200",
+                                        button {
+                                            r#type: "button",
+                                            class: "flex w-full items-center justify-between aria-controls-dis-1 aria-expanded-f rounded-lg py-2 pr-3.5 pl-3 font-bold hover:bg-main-200",
+                                            onclick: move |_| is_contact_menu_open.set(!is_contact_menu_open()),
+                                            "Contact"
+                                            svg {
+                                                class: "size-5 flex-none text-gray-400 aria-hidden data-slot",
+                                                view_box: "0 0 20 20",
+                                                fill: "currentColor",
+                                                path {
+                                                    fill_rule: "evenodd",
+                                                    d:"M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z",
+                                                    clip_rule: "evenodd",
+                                                },
+                                            },
+                                        },
+                                        if is_contact_menu_open() {
+                                            div {
+                                                class: "mt-2 space-y-2",
+                                                id: "disclosure-1",
+                                                a {
+                                                    class: "block rounded-lg py-2 pr-3 pl-6 font-bold hover:bg-main-200",
+                                                    href: "/contact",
+                                                    "Contact"
+                                                },
+                                                div {
+                                                    class: "ml-2",
+                                                    a {
+                                                    class: "block rounded-lg py-2 pr-3 pl-6 font-medium hover:bg-main-200",
+                                                    href: "/contact/report",
+                                                    "Report"
+                                                    },
+                                                    a {
+                                                    class: "block rounded-lg py-2 pr-3 pl-6 font-medium hover:bg-main-200",
+                                                    href: "/contact/info",
+                                                    "Information"
+                                                    },
+                                                },
+                                            },
                                         },
                                     },
                                 },
                                 div {
-                                    class: "mt-2 space-y-2",
-                                    id: "disclosure-1",
+                                    class: "py-6",
                                     a {
-                                        class: "block rounded-lg py-2 pr-3 pl-6 font-bold hover:bg-main-200",
-                                        href: "/contact",
-                                        "Contact"
+                                        class: "-mx-3 block rounded-lg px-3 py-2.5 font-medium hover:bg-main-200",
+                                        href: "/login",
+                                        "LogIn"
                                     },
-                                    div {
-                                        class: "ml-2",
-                                        a {
-                                        class: "block rounded-lg py-2 pr-3 pl-6 font-medium hover:bg-main-200",
-                                        href: "/contact/report",
-                                        "Report"
-                                        },
-                                        a {
-                                        class: "block rounded-lg py-2 pr-3 pl-6 font-medium hover:bg-main-200",
-                                        href: "/contact/info",
-                                        "Information"
-                                        },
-                                    },
-                                },
+                                }
                             },
                         },
-                        div {
-                            class: "py-6",
-                            a {
-                                class: "-mx-3 block rounded-lg px-3 py-2.5 font-medium hover:bg-main-200",
-                                href: "/login",
-                                "LogIn"
-                            },
-                        }
                     },
                 },
             },
