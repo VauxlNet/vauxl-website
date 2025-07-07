f0a0f20aff47d9a1b11ad50ecb5250f9a1f8c6e7
zphnx <92054456+newborn-phoenix@users.noreply.github.com>
2025-07-07 17:12:31 +0200

    Hero Section finished i hope

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,25 +1,37 @@
 use dioxus::prelude::*;
 
+const TEMP_IMAGE: Asset = asset!("/assets/image.webp");
+
 #[component]
 pub fn Home() -> Element {
     rsx! {
         div{
+            id: "hero",
             class: "flex flex-col gap-2",
             div {
-                class: "container mx-auto p-6 bg-main-100 rounded-lg shadow-xl mt-10 mb-2 grid gap-2 grid-cols-8 grid-rows-5",
-                h1 {
-                    class: "text-5xl mb-2 font-bold text-main-300 text-left row-2 col-span-2",
-                    "Vauxl:"
+                class: "container mx-auto p-6 bg-main-100 rounded-lg shadow-xl mt-10 mb-2 grid grid-cols-2 gap-4 h-[600px]",
+                div {
+                    class: "col-span-1 flex flex-col justify-center p-4",
+                    h1{
+                        class: "text-5xl mb-2 font-bold text-main-300 text-left",
+                        "Vauxl:",
+                    },
+                    h1 {
+                        class: "text-4xl font-bold text-main-300 text-left",
+                        "Your Communication, Reclaimed!",
+                    },
+                    h1 {
+                        class: "text-4xl font-bold text-main-300 text-left",
+                        "Private, Secure, Customizable.",
+                    },
+                },
+                div{
+                    class: "col-span-1 relative flex items-center justify-center hidden md:flex",
+                    img{
+                        class: "absolute right-[50px] top-1/2 -translate-y-1/2 w-[800px] h-[450px] bg-black shadow-2xl rounded-lg overflow-hidden perspective-[75em] -rotate-y-20",
+                        src: TEMP_IMAGE,
+                    },
                 }
-                h1 {
-                    class: "text-4xl font-bold text-main-300 text-left row-3 col-span-3",
-                    "Your Communication, Reclaimed!"
-                }
-                h1 {
-                    class: "text-4xl font-bold text-main-300 text-left row-4 col-span-4",
-                    "Private, Secure, Customizable."
-                }
-                
             },
             div {
                 class: "container mx-auto grid grid-cols-6 text-main-300 -mt-2",
